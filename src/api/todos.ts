a963f5ff490c108408ebb919594ddf66271022c6
Ivan Herhi <ionherhi@gmail.com>
2025-05-10 18:19:33 +0200

    ver-02

--- src/api/todos.ts
+++ src/api/todos.ts
@@ -1,17 +1,15 @@
 import { Todo } from '../types/Todo';
 import { client } from '../utils/fetchClient';
 
-export const USER_ID = 0;
-
-export const getTodos = () => {
-  return client.get<Todo[]>(`/todos?userId=${USER_ID}`);
+export const getTodos = (id: number) => {
+  return client.get<Todo[]>(`/todos?userId=${id}`);
 };
 
 export const postTodos = (id: number, data: string) => {
   return client.post<Todo[]>(`/todos?userId=${id}`, data);
 };
 
-export const potchTodos = (id: number, data: string) => {
+export const patchTodos = (id: number, data: string) => {
   return client.patch<Todo[]>(`/todos?userId=${id}`, data);
 };
 
